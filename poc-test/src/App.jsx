82abb020fc4f1da9df853357404db50b3343934a
Bruno Ramires <brunormferreira@hotmail.com>
2019-11-25 13:21:13 -0300

    Update App.jsx

--- poc-test/src/App.jsx
+++ poc-test/src/App.jsx
@@ -3,24 +3,26 @@ import './App.css';
 import { MyContextProvider, useMyContext } from './context/index';
 
 const App = () => {
+  // extract to a new component
   const { state, dispatch } = useMyContext();
   const { count } = state;
   
   const handleIncrement = () => {
-    dispatch({ type: 'INCREMENT' });
+    dispatch({ type: 'INCREMENT_ONE' });
   }
   
   const handleDecrement = () => {
-    dispatch({ type: 'DECREMENT' });
+    dispatch({ type: 'DECREMENT', payload: 5 });
   }
   return (
     <MyContextProvider>
+      // extract to a new component
       <div className="App">
         <h2>Counter</h2>
         <div>
-          <button onClick={handleIncrement}>-</button>
+          <button onClick={handleIncrement}>+</button>
           <span>{count}</span>
-          <button onClick={handleDecrement}>+</button>
+          <button onClick={handleDecrement}>-</button>
         </div>
       </div>
     </MyContextProvider>
