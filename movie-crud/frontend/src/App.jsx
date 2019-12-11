70209cd0502b29a571fd9763351d9ffeba2d9aac
Bruno Ramires <brunormferreira@hotmail.com>
2019-12-10 21:02:47 -0300

    add: change app component to get data from service

--- movie-crud/frontend/src/App.jsx
+++ movie-crud/frontend/src/App.jsx
@@ -1,37 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import './App.css'
 
 import MovieTable from './components/movieTable';
 import * as MoviesServices from './services/api'
 
 const App = () => {
-  const initialMovieState = [
-    {
-    "id": 1,
-    "title": "X-MEN: APOCALYPSE",
-    "genre": "Action",
-    "year": "2016",
-    "producer": "Fox",
-    "poster": "https://upload.wikimedia.org/wikipedia/pt/thumb/e/e6/Xmen_apocalypse_ver18.jpg/250px-Xmen_apocalypse_ver18.jpg"
-    },
-    {
-    "id": 2,
-    "title": "Friday 13",
-    "genre": "Horror",
-    "year": "2016",
-    "producer": "X",
-    }
-  ]
-
-  const [movies, setMovies] = useState(initialMovieState)
-  const [data, setData] = useState({})
+  const [movies, setMovies] = useState([])
 
   useEffect(() => {
     MoviesServices
       .getAllMovies()
-      .then(r => {
-        setData(r.data)
-        console.log(r)
-        console.log(data)
+        .then(data => {
+        setMovies(data)
     })
   }, [])
 
