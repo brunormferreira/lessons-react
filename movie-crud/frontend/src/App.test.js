db6174cc8ecba2402238f3eda326871ffbcf89ac
Bruno Ramires <brunormferreira@hotmail.com>
2019-12-13 10:05:51 -0300

    add: trying to do more tests

--- movie-crud/frontend/src/App.test.js
+++ movie-crud/frontend/src/App.test.js
@@ -2,16 +2,27 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+// import MovieTable from './components/movieTable';
+// import EditMovieForm from './components/editMovieForm'
+
 describe('tests about app component', () => {
   test('render crud title', () => {
     const { getByText } = render(<App />);
     const linkElement = getByText(/CRUD/i);
     expect(linkElement).toBeInTheDocument();
   });
-  
+
   test('render movie table title', () => {
     const { getByText } = render(<App />);
     const linkElement = getByText(/View/i);
     expect(linkElement).toBeInTheDocument();
   });
+  
+  // test('render movie table component', () => {
+  //   const { getByPlaceholderText} = render(<MovieTable />);
+  //   const linkElement = getByPlaceholderText(/enter/i);
+  
+  //   expect(linkElement).toBe(true);
+  // })
+
 });
